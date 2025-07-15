45361219d19aed0bfb918ca4b4502dfb96dc27cf
Mateus Andrade <mateus.nascimento.andrade@gmail.com>
2025-07-15 05:58:36 -0300

    Sessao quem sou eu criada

--- resources/views/components/em-construcao.blade.php
+++ resources/views/components/em-construcao.blade.php
@@ -1,6 +1,11 @@
-<div class="fixed inset-0 z-40 pointer-events-none">
-    <div
-        class="w-[200%] rotate-[-25deg] bg-yellow-400 text-black text-5xl font-extrabold text-center py-4 shadow-xl uppercase absolute top-1/2 left-[-50%]">
+<div class="fixed inset-0 z-40 pointer-events-none overflow-hidden">
+    <div class="absolute top-1/2 left-1/2
+             w-[220vw] md:w-[200%]
+             -translate-x-1/2 -translate-y-1/2
+             rotate-[55deg] md:rotate-[-25deg]
+             bg-yellow-400 text-black
+             text-3xl md:text-5xl
+             font-extrabold text-center py-4 shadow-xl uppercase">
         🚧 Em Construção 🚧
     </div>
 </div>
--- resources/views/components/quem-sou-eu.blade.php
+++ resources/views/components/quem-sou-eu.blade.php
@@ -1,8 +1,16 @@
-<section class=" min-h-[40vh] flex items-center justify-start font-marvel">
-    <div class="avatar ml-60">
+<section class="min-h-[40vh] flex flex-col md:flex-row items-center justify-center font-marvel gap-10 md:gap-20">
+    <div class="avatar">
         <div class="ring-white ring-offset-base-150 w-48 h-48 rounded-full ring-2 ring-offset-2 overflow-hidden">
-            <img src="img/mateus-avatar.jpg" class="object-cover w-full h-full" alt="Avatar Mateus" />
+            <img src="img/mateus-avatar.jpg" class="object-cover w-full h-full" alt="Avatar Mateus Andrade" />
         </div>
     </div>
-
+    <div class="max-w-xl px-4 md:px-0">
+        <h2 class="text-3xl font-bold mb-4 text-center md:text-left">Quem Sou Eu?</h2>
+        <p class="text-base leading-relaxed text-justify">
+            Sou desenvolvedor Fullstack com foco em Java (Spring, JSF, Thymeleaf), e buscando novos conhecimentos
+            constantes que estou atualmente estudando PHP com Laravel e futuramente Angular. Tenho paixão por
+            programação, tecnologia — esta paixão me levou a realizar transição de carreira da área de Recursos Humanos
+            para desenvolvimento, que continuo até hoje.
+        </p>
+    </div>
 </section>
